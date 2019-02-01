4409bb776f328a70b333f3d9280b7bc4c1ba8ab5
meta-sakshi-gupta <46710040+meta-sakshi-gupta@users.noreply.github.com>
2019-02-01 16:52:12 +0530

    Update QueensMain.java

--- Assignment5/NQueen/QueensMain.java
+++ Assignment5/NQueen/QueensMain.java
@@ -12,7 +12,21 @@ public class QueensMain
   Position [] position = new Position [order];
   SolveNQueenOneSolution NQueen = new SolveNQueenOneSolution();
   position = NQueen.solveNQueenOneSolution(order);
-  Arrays.stream(position).forEach(pos -> System.out.println(pos.row + " " + pos.col));
+  int[][] array=new int[order][order];
+  for(int row =0;row<order;row++)
+  {
+	  for(int col=0;col<order;col++)
+	  if (col==position[row].col)
+	   array[row][col]=1;
+	  else
+  	array[row][col]=0;
   }
-
+  System.out.println("possible solution is : ");
+  for(int row=0;row<order;row++)
+  {
+   for(int col=0;col<order;col++)
+	   System.out.print(array[row][col]+"  ");
+   System.out.print("\n");
+  }
+ }
 }
